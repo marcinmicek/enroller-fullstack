6908809133b8bbf8ce75f6c1049dab91afe90692
student3 <student3@pcoip.ki.agh.edu.pl>
2018-06-09 10:29:29 +0200

    Initial version

--- src/main/frontend/src/App.vue
+++ src/main/frontend/src/App.vue
@@ -11,7 +11,16 @@
       <meetings-page :username="authenticatedUsername"></meetings-page>
     </div>
     <div v-else>
-      <login-form @login="login($event)"></login-form>
+      <button :class="isRegistering ? 'button-outline' : ''"
+              @click="isRegistering = false">Zaloguj się</button>
+      <button :class="!isRegistering ? 'button-outline' : ''"
+              @click="isRegistering = true">Zarejestruj się</button>
+      <login-form @login="login($event)"
+                  v-if="!isRegistering"></login-form>
+      <login-form @login="register($event)"
+                  :button-label="'Zarejestruj się'"
+                  v-else="isRegistering"></login-form>
+
     </div>
   </div>
 </template>
@@ -25,12 +34,16 @@
         components: {LoginForm, MeetingsPage},
         data() {
             return {
-                authenticatedUsername: ""
+                authenticatedUsername: "",
+                isRegistering: false
             };
         },
         methods: {
             login(user) {
                 this.authenticatedUsername = user.login;
+            },
+            register(user) {
+                alert(user.login);
             },
             logout() {
                 this.authenticatedUsername = '';
